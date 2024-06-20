1108def9fa2d92590f2eb091bd247129354a74f9
Piotrek <252760@edu.p.lodz.pl>
2024-06-20 21:48:05 +0200

    geometry.h

--- geometry.h
+++ geometry.h
@@ -4,5 +4,32 @@
 
 #ifndef GEOMETRIA_GEOMETRY_H
 #define GEOMETRIA_GEOMETRY_H
+using namespace std;
 
+class Square
+{
+private:
+    int length, area;
+
+public:
+    Square(int length = 0);
+    int getLength()const;
+    void setLength(int length);
+    int getArea()const;
+    int getPerimeter()const;
+};
+
+class Circle
+{
+private:
+    double radius, area;
+
+public:
+    Circle(double radius = 0);
+
+    double getRadius()const;
+    void setRadius(double radius);
+    double getArea()const;
+    double getPerimeter()const;
+};
 #endif //GEOMETRIA_GEOMETRY_H
